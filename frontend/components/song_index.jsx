c9d21f00bbb8ff29cb832c9715d46b4349811b76
Samuel Chia <Samuel Chia>
2018-02-11 02:40:20 -0800

    change post date to show how long ago

--- frontend/components/song_index.jsx
+++ frontend/components/song_index.jsx
@@ -20,15 +20,42 @@ class SongIndex extends React.Component {
         <ul className="songs-index-list">
           {
             songs.map(song => {
-              let postDate = song.created_at.slice(0,10).split("-");
-              postDate.push(postDate.shift());
-              postDate = postDate.join("-");
+              let postDate = new Date(song.created_at);
+              let now = new Date();
+              let elapsedTime = (now - postDate) / 8.64e7;
+              let timeString = " day";
+              if (elapsedTime > 30) {
+                elapsedTime = elapsedTime / 30;
+                timeString = " month";
+                if (elapsedTime >= 12) {
+                  elapsedTime = elapsedTime / 12;
+                  timeString = " year";
+                }
+              } else if (elapsedTime < 1) {
+                elapsedTime = elapsedTime * 24;
+                timeString = " hour";
+                if (elapsedTime < 1) {
+                  elapsedTime = elapsedTime * 60;
+                  timeString = " minute";
+                  if (elapsedTime < 1) {
+                    timeString = "less than 1 minute";
+                  }
+                }
+              }
+              elapsedTime = Math.floor(elapsedTime);
+              if (elapsedTime > 0) {
+                timeString = elapsedTime + timeString;
+                if (elapsedTime > 1) {
+                  timeString += "s";
+                }
+              }
+              timeString+= " ago";
               return (
                 <li className="song-index-list-item" key={song.id}>
                   <div className="song-list-user-div">
                     <Link to={song.author_url}><img className="song-list-user-img" src={song.author_picture_url} /></Link>
                     <span className="song-list-user-span">
-                      <Link to={song.author_url}>{song.author_name}</Link><span className="song-list-user-span-inner">posted a track on {postDate}</span>
+                      <Link to={song.author_url}>{song.author_name}</Link><span className="song-list-user-span-inner">posted a track {timeString}</span>
                     </span>
                   </div>
                   <SongItemContainer song={song} path="stream" playlist="allSongs" />
