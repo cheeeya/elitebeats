4a79dfe80c2eaa64c124dd102798497db3f18cae
Samuel Chia <cheeeya@berkeley.edu>
2018-03-09 21:30:59 -0800

    add comment migration

--- db/migrate/20180310052610_create_comments.rb
+++ db/migrate/20180310052610_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
-
+      t.text :body, null: false
+      t.integer :author_id, null: false
+      t.integer :song_id, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180211140801) do
+ActiveRecord::Schema.define(version: 20180310052610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "author_id", null: false
+    t.integer "song_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string "title", null: false
