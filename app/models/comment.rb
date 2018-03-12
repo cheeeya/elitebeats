d2f2b442d12d082c1af0554d20bc6cb70211fe2e
Samuel Chia <cheeeya@berkeley.edu>
2018-03-11 22:56:40 -0700

    fix syntax errors and annotate files

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :text             not null
+#  author_id  :integer          not null
+#  song_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Comment < ApplicationRecord
   validates :author_id, :song_id, :body, presence: true
 
@@ -6,7 +18,7 @@ class Comment < ApplicationRecord
     foreign_key: :song_id,
     class_name: 'Song'
 
-  belongs_to :author
+  belongs_to :author,
     primary_key: :id,
     foreign_key: :author_id,
     class_name: 'User'
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :text             not null
+#  author_id  :integer          not null
+#  song_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
