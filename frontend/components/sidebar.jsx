29a7e155087eb90434c02b1f715cca0458445ec4
Samuel Chia <cheeeya@berkeley.edu>
2018-03-20 23:31:35 -0700

    add styling to sidebar

--- frontend/components/sidebar.jsx
+++ frontend/components/sidebar.jsx
@@ -9,15 +9,17 @@ class Sidebar extends React.Component {
     return(
       <section className="sidebar">
         <div className="wrapper-sidebar">
-          <div className="sidebar-follow"><div id="sidebar-icon-follow"/>Who to follow</div>
-          <div className="sidebar-likes"><div id="sidebar-icon-likes"/>likes</div>
-          <div className="sidebar-history"><div id="sidebar-icon-history"/>Listening history</div>
-          <footer>
-            <a>Legal</a>
-            <a>Privacy</a>
-            <a>Cookies</a>
-            <a>Imprint</a>
-            <a>Popular searches</a>
+          <div className="sidebar-follow sidebar-header"><div id="sidebar-icon-follow"/>Who to follow</div>
+          <div className="sidebar-content">
+          </div>
+          <div className="sidebar-likes sidebar-header"><div id="sidebar-icon-likes"/>likes</div>
+          <div className="sidebar-content">
+          </div>
+          <div className="sidebar-history sidebar-header"><div id="sidebar-icon-history"/>Listening history</div>
+          <div className="sidebar-content">
+          </div>
+          <footer className="sidebar-footer">
+            <a>Legal</a> - <a>Privacy</a> - <a>Cookies</a> - <a>Imprint</a> - <a>Popular searches</a>
           </footer>
         </div>
       </section>
