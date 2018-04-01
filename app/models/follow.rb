3425c6e47abbd8bf147c57c66a71543a962519e0
Samuel Chia <cheeeya@berkeley.edu>
2018-03-31 21:15:32 -0700

    add follower/following many-to-many association

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -10,4 +10,13 @@
 #
 
 class Follow < ApplicationRecord
+  belongs_to :following,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: 'User'
+
+  belongs_to :follower,
+    primary_key: :id,
+    foreign_key: :follower_id,
+    class_name: 'User'
 end
