e410d1840606773b4233fbbbdc4b57c97a1a5f6a
Samuel Chia <cheeeya@berkeley.edu>
2018-04-02 22:07:32 -0700

    add unfiltered user content to follow list

--- app/views/api/users/index.json.jbuilder
+++ app/views/api/users/index.json.jbuilder
@@ -1,6 +1,6 @@
 @users.each do |user|
   json.set! user.id do
-    json.extract! user, :profile_url, :display_name
+    json.extract! user, :id, :profile_url, :display_name
     if user.s_prof_pic_url
       json.profile_picture_url user.s_prof_pic_url
     else
--- frontend/components/sidebar.jsx
+++ frontend/components/sidebar.jsx
@@ -10,45 +10,66 @@ class Sidebar extends React.Component {
   }
 
   render() {
+    const { userList } = this.props;
+    // let suggestionList = Array.new(3);
+    let suggestionList = Object.values(userList).slice(0, 3);
+
+    // let followContent = <li className="sidebar-follow-user-item">
+    //                       <div className="follow-list-user-avatar"/>
+    //                       <div className="follow-list-user-info">
+    //                         <div className="follow-list-username">
+    //                           <span>user1</span>
+    //                         </div>
+    //                         <div className="follow-list-user-data">
+    //                           <button className="follow-button" type="button">Follow</button>
+    //                         </div>
+    //                       </div>
+    //                     </li>
+    //                     <li className="sidebar-follow-user-item">
+    //                       <div className="follow-list-user-avatar"/>
+    //                       <div className="follow-list-user-info">
+    //                         <div className="follow-list-username">
+    //                           <span>user2</span>
+    //                         </div>
+    //                         <div className="follow-list-user-data">
+    //                           <button className="follow-button" type="button">Follow</button>
+    //                         </div>
+    //                       </div>
+    //                     </li>
+    //                     <li className="sidebar-follow-user-item">
+    //                       <div className="follow-list-user-avatar"/>
+    //                       <div className="follow-list-user-info">
+    //                         <div className="follow-list-username">
+    //                           <span>user3</span>
+    //                         </div>
+    //                         <div className="follow-list-user-data">
+    //                           <button className="follow-button" type="button">Follow</button>
+    //                         </div>
+    //                       </div>
+    //                     </li>
+
     return(
       <section className="sidebar">
         <div className="sidebar-wrapper">
           <div className="sidebar-follow sidebar-header"><div id="sidebar-icon-follow"/>Who to follow</div>
           <div className="sidebar-content">
             <ul className="sidebar-follow-user-list">
-              <li className="sidebar-follow-user-item">
-                <div className="follow-list-user-avatar"/>
-                <div className="follow-list-user-info">
-                  <div className="follow-list-username">
-                    <span>user1</span>
-                  </div>
-                  <div className="follow-list-user-data">
-                    <button className="follow-button" type="button">Follow</button>
-                  </div>
-                </div>
-              </li>
-              <li className="sidebar-follow-user-item">
-                <div className="follow-list-user-avatar"/>
-                <div className="follow-list-user-info">
-                  <div className="follow-list-username">
-                    <span>user2</span>
-                  </div>
-                  <div className="follow-list-user-data">
-                    <button className="follow-button" type="button">Follow</button>
-                  </div>
-                </div>
-              </li>
-              <li className="sidebar-follow-user-item">
-                <div className="follow-list-user-avatar"/>
-                <div className="follow-list-user-info">
-                  <div className="follow-list-username">
-                    <span>user3</span>
-                  </div>
-                  <div className="follow-list-user-data">
-                    <button className="follow-button" type="button">Follow</button>
-                  </div>
-                </div>
-              </li>
+              {
+                suggestionList.map(user => (
+                  <li className="sidebar-follow-user-item" key={user.id}>
+                    <div className="follow-list-user-avatar" style={{ backgroundImage: `url(${user.profile_picture_url})` }}/>
+                    <div className="follow-list-user-info">
+                      <div className="follow-list-username">
+                        <span>{user.display_name}</span>
+                      </div>
+                      <div className="follow-list-user-data">
+                        <button className="follow-button" type="button">Follow</button>
+                      </div>
+                    </div>
+                  </li>
+                  )
+                )
+              }
             </ul>
           </div>
           <div className="sidebar-likes sidebar-header"><div id="sidebar-icon-likes"/>likes</div>
