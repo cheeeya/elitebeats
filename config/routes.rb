0ddb279e3135cd387643a8e25df741559f7b4cbd
Samuel Chia <cheeeya@berkeley.edu>
2018-04-13 15:21:34 -0700

    nest follows create route under users

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:index, :create, :destroy, :update]
+    resources :users, only: [:index, :create, :destroy, :update] do
+      resources :follows, only: [:create]
+    end
     resource :session, only: [:create, :destroy]
     resources :songs, only: [:index, :create, :destroy, :update] do
       resources :comments, only: [:create]
     end
+    resources :follows, only: [:destroy]
     resources :comments, only: [:destroy]
-    resources :follows, only: [:create, :destroy]
     post '/users/find', to: 'users#verify_user_exists'
     get '/users/:profile_url', to: 'users#get_user'
     get '/users/:profile_url/songs/:permalink', to: 'songs#fetch_song'
