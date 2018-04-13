cb4aaf4e56297d048ccaf5c21c8377680612d022
Samuel Chia <cheeeya@berkeley.edu>
2018-04-13 15:04:24 -0700

    add first implementation of create and destroy methods

--- app/controllers/api/follows_controller.rb
+++ app/controllers/api/follows_controller.rb
@@ -1,6 +1,23 @@
 class Api::FollowsController < ApplicationController
 
   def create
+    @follow = Follow.new(follow_params)
+    @follow.follower_id = current_user.id
+    if @follow.save
+      render json: ["followed"], stats: 200
+    else
+      render json: @follow.errors.full_messages, status: 422
+    end
+  end
 
+  def destroy
+    @follow = Follow.find(aprams[:id])
+    @follow.delete
+    render json: ["unfollowed"], status: 200
+  end
+
+  private
+  def follow_params
+    params.require(:follow).permit(:user_id)
   end
 end
